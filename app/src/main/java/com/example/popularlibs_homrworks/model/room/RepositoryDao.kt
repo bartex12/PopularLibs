81c7bf749e5d292a67d07338fb6058854a20b0d4
пк <barcats@inbox.ru>
2021-01-23 15:53:59 +0300

    классы и репозитории для Room

---
+++ app/src/main/java/com/example/popularlibs_homrworks/model/room/RepositoryDao.kt
@@ -0,0 +1,45 @@
+package com.example.popularlibs_homrworks.model.room
+
+import androidx.room.*
+
+/*
+*  стандартные CRUD разных вариаций для создания, чтения, обновления и удаления данных, а также
+* возможность поискать пользователя по логину. С помощью встроенного шаблонизатора содержимое
+*  аргумента login функции findByLogin подставится вместо :login в запрос. В функциях insert
+* с помощью аргумента аннотации onConflict мы указываем, что при возникновении конфликта
+* по первичному ключу необходимо заменить старое значение новым*/
+@Dao
+interface RepositoryDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(user: RoomGithubRepository)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(vararg users: RoomGithubRepository)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(users: List<RoomGithubRepository>)
+
+    @Update
+    fun update(user: RoomGithubRepository)
+
+    @Update
+    fun update(vararg users: RoomGithubRepository)
+
+    @Update
+    fun update(users: List<RoomGithubRepository>)
+
+    @Delete
+    fun delete(user: RoomGithubRepository)
+
+    @Delete
+    fun delete(vararg users: RoomGithubRepository)
+
+    @Delete
+    fun delete(users: List<RoomGithubRepository>)
+
+    @Query("SELECT * FROM RoomGithubRepository")
+    fun getAll(): List<RoomGithubRepository>
+
+    @Query("SELECT * FROM RoomGithubRepository WHERE userId = :userId")
+    fun findForUser(userId: String): List<RoomGithubRepository>
+}
